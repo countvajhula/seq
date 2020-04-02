70e3d9349d71a18beb481cf6212c4f8886c389de
Siddhartha <sid@countvajhula.com>
2020-04-02 12:23:47 -0700

    tree-map, -filter and -fold

--- utils.rkt
+++ utils.rkt
@@ -136,6 +136,23 @@
                                                  'level)
                                #:converse? boolean?)
                               sequence?)]
+          [tree-map (-> (-> any/c any/c)
+                        sequence?
+                        sequence?)]
+          [tree-filter (-> (-> any/c any/c)
+                           sequence?
+                           sequence?)]
+          [tree-fold (->* ((-> any/c any/c any/c) sequence?)
+                          (any/c
+                           #:order (one-of/c 'pre
+                                             'post
+                                             'in
+                                             'level)
+                           #:converse? boolean?
+                           #:argument-order (one-of/c 'abb
+                                                      'bab)
+                           #:with-steps? boolean?)
+                          any/c)]
           [: (collection? any/c . -> . collection?)]))
 
 (define : conj)
@@ -448,6 +465,38 @@
                seq
                (add-between sep seq))))
 
+(define (tree-map f tree)
+  (if (empty? tree)
+      (stream)
+      (stream-cons (f (first tree))
+                   (map (curry tree-map f)
+                        (rest tree)))))
+
+(define (tree-filter f tree)
+  (if (empty? tree)
+      (stream)
+      (if (f (first tree))
+          (stream-cons (first tree)
+                       (remove-when empty?
+                                    (map (curry tree-filter f)
+                                         (rest tree))))
+          (stream))))
+
+(define (tree-fold f
+                   tree
+                   [base undefined]
+                   #:order [order 'pre]
+                   #:converse? [converse? #f]
+                   #:argument-order [argument-order 'abb]
+                   #:with-steps? [with-steps? #f])
+  (foldl f
+         (tree-traverse tree
+                        #:order order
+                        #:converse? converse?)
+         base
+         #:order argument-order
+         #:with-steps? with-steps?))
+
 (define (tree-traverse-preorder tree
                                 #:converse? [converse? #f])
   (if (empty? tree)
@@ -491,15 +540,17 @@
 
 (define (tree-traverse-levelorder tree
                                   #:converse? [converse? #f])
-  (let loop ([queue (list tree)])
-    (if (empty? queue)
-        (stream)
-        (let ([current (first queue)])
-          (stream-cons (first current)
-                       (loop (d:append (rest queue)
-                                       (if converse?
-                                           (reverse (rest current))
-                                           (rest current)))))))))
+  (if (empty? tree)
+      (stream)
+      (let loop ([queue (list tree)])
+        (if (empty? queue)
+            (stream)
+            (let ([current (first queue)])
+              (stream-cons (first current)
+                           (loop (d:append (rest queue)
+                                           (if converse?
+                                               (reverse (rest current))
+                                               (rest current))))))))))
 
 (define (tree-traverse tree
                        #:order [order 'pre]
