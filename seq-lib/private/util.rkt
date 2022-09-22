0374597b0e35991fe3b152ec7a6c04e2646093d7
Siddhartha <sid@countvajhula.com>
2022-09-21 22:11:44 -0700

    use version-case

--- seq-lib/private/util.rkt
+++ seq-lib/private/util.rkt
@@ -1,9 +1,14 @@
 #lang racket/base
 
 (require syntax/parse/define
+         version-case
          (for-syntax racket/base))
 
 (provide define-alias)
+
+(version-case
+ [(version< (version) "7.9.0.22")
+  (define-alias define-syntax-parse-rule define-simple-macro)])
 
 (define-syntax-parse-rule (define-alias alias:id name:id)
   (define-syntax alias (make-rename-transformer #'name)))
